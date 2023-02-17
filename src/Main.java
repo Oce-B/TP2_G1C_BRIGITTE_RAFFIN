3f29345d46eec70b5fa817e6a83c72c81af01dd4
Oce-B <oceane.brf@outlook.fr>
2023-02-17 15:54:34 +0100

    Fonction discriminant améliorée

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    min();
+        discriminant();
 
 
     }
+        public static void egaliteStr(){
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Veuillez entrer la première chaîne de carctère");
+            String premiereChaine = scanner.next();
+
+            System.out.println("Veuillez entrer la première chaîne de carctère");
+            String deuxiemeChaine = scanner.next();
+
+        }
         public static void max(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Veuillez entrer le premier nombre");
-        int premierNombre = scanner.nextInt();
-        System.out.println("Veuillez entrer un second Nombre");
-        int secondNombre = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Veuillez entrer le premier nombre");
+            int premierNombre = scanner.nextInt();
+            System.out.println("Veuillez entrer un second Nombre");
+            int secondNombre = scanner.nextInt();
 
-        if (premierNombre<secondNombre){
-            System.out.print(secondNombre);
-        } else if (secondNombre==premierNombre){
-            System.out.println("Ils sont égaux");
-        }
-        else{
-            System.out.print(premierNombre);
-        }
+            if (premierNombre<secondNombre){
+                System.out.print(secondNombre);
+            } else if (secondNombre==premierNombre){
+                System.out.println("Ils sont égaux");
+            }
+            else{
+                System.out.print(premierNombre);
+            }
         }
 
 
@@ -43,18 +52,18 @@ public class Main {
     }
 
         public static void parite(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Veuillez entrer un entier");
-        int nombre = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Veuillez entrer un entier");
+            int nombre = scanner.nextInt();
 
-        if (nombre%2==0){
-            System.out.println("Ce nombre est pair");
-            System.out.print(nombre);
-        }
-        else {
-            System.out.println("Ce nombre est impair");
-            System.out.print(nombre);
-        }
+            if (nombre%2==0){
+                System.out.println("Ce nombre est pair");
+                System.out.print(nombre);
+            }
+            else {
+                System.out.println("Ce nombre est impair");
+                System.out.print(nombre);
+                }
         }
 
     public static void discriminant(){
@@ -70,7 +79,12 @@ public class Main {
         int delta = (int) (Math.pow(b,2) - 4 * a *c);
 
         if (delta < 0) {
-            System.out.println("Ce polynome n'a pas de valeur réelle.");
+            double carre = Math.pow(-delta,1/2);
+            String x1 = "(-"+  b + " + i * " + carre + ") / " + 2*a;
+            String x2 =  "(-"+  b + " - i * " + carre + ") / " + 2*a;
+            System.out.println("Les racines complexes de ce polynome sont");
+            System.out.println(x1);
+            System.out.println(x2);
         }
         else if (delta == 0) {
             double x0 = (-b)/2*a;
